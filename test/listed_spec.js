52de4ca84fcc5d3c98add5f8ba5da6a5ee5a4d3d
Kevin Solorio <soloriok@gmail.com>
2014-01-20 19:38:53 -0600

    adding push and isEmpty

---
+++ test/listed_spec.js
@@ -0,0 +1,47 @@
+var listed = require('../lib/listed'),
+    assert = require('assert');
+
+describe('Stack', function(){
+
+  describe('After initialization', function(){
+    it('The stack has a size of 0', function(){
+      var stack = listed.createStack();
+      assert.equal(stack.size(), 0);
+    });
+  });
+
+  describe('#push(item)', function(){
+    it('adds a node to the top of the stack', function(){
+      var stack = listed.createStack();
+      stack.push(1);
+      assert.equal(stack._first.item, 1);
+    });
+
+    it('increments the size of the stack', function(){
+      var stack = listed.createStack();
+      stack.push(1);
+      assert.equal(stack.size(), 1);
+    });
+  });
+
+  describe('#isEmpty', function(){
+
+    describe('when stack is empty', function(){
+      it('returns true', function(){
+        var stack = listed.createStack();
+        assert(stack.isEmpty());
+      });
+    });
+
+    describe('when stack is not empty', function(){
+      it('returns false', function(){
+        var stack = listed.createStack();
+        stack.push(1);
+        assert(!stack.isEmpty());
+      });
+    });
+
+  });
+
+});
+
